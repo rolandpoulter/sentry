16ddd13cc1bcd77558a94c789a0aa17ab91fa925
roland <rolandpoulter@gmail.com>
2013-01-31 17:12:14 -0800

    adds more error catching

--- sentry.js
+++ sentry.js
@@ -3,6 +3,8 @@ var exec = require('child_process').exec,
     file = require('file'),
     fs = require('fs');
 
+// FIXME: callbacks get fire more than once per change
+
 // Watch for changes on a file using a file path or wildcards
 // If passed a task callback is passed (err, stdout, stderr)
 // If task is ommitted then callback is passed (filename)
@@ -12,32 +14,29 @@ var exec = require('child_process').exec,
 // @param {Function} callback (error, filename, [stdout], [stderr])
 // @param {Object} [options]
 // @param {Object} [map]
-exports.watch = function (filename, task, callback, options) {
-	if (typeof task === 'function') callback = task;
+exports.watch = function (filename, task, callback, options, map) {
+	if (typeof task === 'function') {
+		callback = task;
+		task = null;
+	}
 
-	var map = {};
+	map = map || {};
 
 	if (filename.indexOf('/*') !== -1) {
 		// Get the files we want to catch with the wildcards, and watch them
 		exports.findWildcards(filename, function (error, files) {
 			if (error) {
-				return callback(error);
+				return callback(error, filename);
 			}
 
 			files.forEach(function (file) {
 				watchFile(file, task, callback, options, map);
 			});
-		})
+		}, map)
 
 	} else {
 		// If the file is a string without wildcards, watch just that file
-		fs.exists(filename, function (exists) {
-			if (!exists) {
-				return callback(new Error('Sentry2.watch file: "' + filename + '" does not exist!'));
-			}
-
-			watchFile(filename, task, callback, options, map);
-		});
+		watchFile(filename, task, callback, options, map);
 	}
 
 	return map;
@@ -64,29 +63,42 @@ exports.watchRegExp = function (dir, regex, task, callback, options, map) {
 	map = map || {};
 
 	// Watch the matches files
-	file.walk(dir, function (error, start, dirs, files) {
-		if (error) {
-			return callback(error);
-		}
+	try {
+		file.walk(dir, function (error, start, dirs, files) {
+			if (error) {
+				return callback(error, dir);
+			}
 
-		dirs.forEach(function (dir) {
-			watchFile(start + '/' + dir, task, function (dirname) {
-				if (regex.test(dirname)) {
-					callback(null, dirname);
-
-					exports.watchRegExp(dir, regex, task, callback, options, map);
+			dirs.forEach(function (dir) {
+				if (dir.indexOf(start) === -1) {
+					dir = start + '/' + dir;
 				}
-			}, options, map)
+
+				watchFile(file, task, function (dirname) {
+					if (regex.test(dirname)) {
+						callback(null, dirname);
+
+						exports.watchRegExp(dir, regex, task, callback, options, map);
+					}
+				}, options, map)
+			});
+
+			files.forEach(function (file) {
+				if (file.indexOf(start) === -1) {
+					file = start + '/' + file;
+				}
+
+				watchFile(file, task, function (filename) {
+					if (regex.test(filename)) {
+						callback(null, filename);
+					}
+				}, options, map);
+			})
 		});
 
-		files.forEach(function (file) {
-			watchFile(start + '/' + file, task, function (filename) {
-				if (regex.test(filename)) {
-					callback(null, filename);
-				}
-			}, options, map);
-		})
-	});
+	} catch (error) {
+		callback(error, dir);
+	}
 
 	return map;
 };
@@ -99,53 +111,65 @@ exports.watchRegExp = function (dir, regex, task, callback, options, map) {
 // @param {Object} [options]
 // @param {Object} [map]
 function watchFile (filename, task, callback, options, map) {
-	var listener;
-
-	if (map && map[filename]) return;
-
-	if (fs.watch) {
-		fs.watch(filename, listener = function (event, _filename) {
-			_filename = _filename || filename;
-
-			if (event === 'rename') {
-				if (map[_filename]) map[_filename]();
-
-				watchFile(_filename, task, callback, options, map);
-			}
-
-			finish(_filename);
-		});
-
-	} else {
-		fs.watchFile(filename, listener = function (curr, prev) {
-			if (curr.size === prev.size && curr.mtime.getTime() === prev.mtime.getTime()) {
-				return;
-			}
-
-			finish(filename);
-		});
-
-		function finish (filename) {
-			if (typeof task === 'string') {
-				exec(task, function (err, stdout, stderr) {
-					if (callback) {
-						callback(err, filename, stdout, stderr);
-					} else {
-						console.log(stdout);
-						if (err) {
-							console.log(err);
-						}
-					}
-				});
-
-			} else if (callback) {
-				callback(null, filename);
-			}
+	fs.exists(filename, function (exists) {
+		if (!exists) {
+			return callback(new Error('Sentry2.watch file: "' + filename + '" does not exist!'), filename);
 		}
 
-		if (map) map[filename] = function () {
-			fs.unwatchFile(filename, listener);
-		};
+		var listener;
+
+		if (map && map[filename]) return;
+
+		try {
+			if (fs.watch) {
+				fs.watch(filename, listener = function (event, _filename) {
+					_filename = _filename || filename;
+
+					if (event === 'rename') {
+						if (map && map[_filename]) map[_filename]();
+
+						watchFile(_filename, task, callback, options, map);
+					}
+
+					finish(_filename);
+				});
+
+			} else {
+				fs.watchFile(filename, listener = function (curr, prev) {
+					if (curr.size === prev.size && curr.mtime.getTime() === prev.mtime.getTime()) {
+						return;
+					}
+
+					finish(filename);
+				});
+			}
+		} catch (error) {
+			callback(error, filename);
+		}
+
+		if (map) {
+			map[filename] = function () {
+				fs.unwatchFile(filename, listener);
+			};
+		}
+	});
+
+	function finish (filename) {
+		if (typeof task === 'string') {
+			exec(task, function (error, stdout, stderr) {
+				if (callback) {
+					callback(error, filename, stdout, stderr);
+				} else {
+					console.log(stdout);
+					if (error) {
+						console.log(error);
+					}
+				}
+			});
+
+		} else if (callback) {
+			callback(null, filename);
+		}
 	}
 }
 
@@ -155,7 +179,7 @@ function watchFile (filename, task, callback, options, map) {
 //
 // @param {String} filename
 // @param {Function} callback (error, files)
-exports.findWildcards = function (filename, callback) {
+exports.findWildcards = function (filename, callback, map) {
 	var split,
 	    root,
 	    ext;
@@ -164,6 +188,9 @@ exports.findWildcards = function (filename, callback) {
 		return callback(new Error('Sentry2.findWildcards was provided an invalid filename: ' + filename));
 	}
 
+	map = map || {};
+	map.__map = map.__map || {};
+
 	// If there is a wildcard in the /**/* form of a file then remove it and
 	// splice in all files recursively in that directory
 	if (filename.indexOf('**/*') !== -1) {
@@ -171,28 +198,42 @@ exports.findWildcards = function (filename, callback) {
 		root = split[0];
 		ext = split[1];
 
-		file.walk(root, function (error, start, dirs, files) {
-			if (error) {
-				return callback(error);
-			}
+		walkDir();
+		watchFile(root, null, walkDir, {persistent: false}, map.__map);
 
-			callback(null, filterList(start, dirs.concat(files)));
-		});
+		function walkDir () {
+			try {
+				file.walk(root, function (error, start, dirs, files) {
+					if (error) {
+						return callback(error, root);
+					}
+
+					callback(null, filterList(start, dirs.concat(files)));
+				});
+			} catch (error) {
+				callback(error, root);
+			}
+		}
 
 	// If there is a wildcard in the /* form then remove it and splice in all the
 	// files one directory deep
 	} else if (filename.indexOf('/*')) {
-		split = filename.spit('/*');
+		split = filename.split('/*');
 		root = split[0];
 		ext = split[1];
 
-		fs.readdir(root, function (error, files) {
-			if (error) {
-				return callback(error);
+		scanDir();
+		watchFile(root, null, scanDir, {persistent: false}, map.__map);
 
-				callback(null, filterList(root, files));
-			}
-		});
+		function scanDir () {
+			fs.readdir(root, function (error, files) {
+				if (error) {
+					return callback(error, root);
+
+					callback(null, filterList(root, files));
+				}
+			});
+		}
 
 	} else {
 		callback(null, []);
@@ -200,19 +241,23 @@ exports.findWildcards = function (filename, callback) {
 
 	function filterList (start, list) {
 		var filtered = [],
-		    map = {};
+		    _map = {};
 
 		if (start.charAt(start.length - 1) !== '/') {
 			start += '/';
 		}
 
 		list.forEach(function (name) {
-			if (new RegExp(ext + '$').test(name) && !map[start + name]) {
-				name = start + name;
+			if (new RegExp(ext + '$').test(name)) {
+				if (name.indexOf(start) === -1) {
+					name = start + name;
+				}
 
-				map[name] = true;
+				if (!_map[name]) {
+					_map[name] = true;
 
-				filtered.push(name);
+					filtered.push(name);
+				}
 			}
 		});
 
